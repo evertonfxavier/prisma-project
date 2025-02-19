6917a54f4f9ada2fdeec5378c9c9dc68fd371183
Everton Xavier <evertonfxavier@gmail.com>
2025-02-19 15:30:25 -0300

    update with relating item to user

--- index.ts
+++ index.ts
@@ -13,20 +13,18 @@ async function main() {
   //   },
   // });
   // console.log("Usuário criado:", user);
-
   //UPDATE
   //Atualizar endereço de um usuário:
-  await prisma.user.update({
-    where: { email: "jose@gmail.com" },
-    data: {
-      address: {
-        rua: "Rua das Pedras",
-        numero: 33,
-        cidade: "São Carlos",
-      },
-    },
-  });
-
+  // await prisma.user.update({
+  //   where: { email: "jose@gmail.com" },
+  //   data: {
+  //     address: {
+  //       rua: "Rua das Pedras",
+  //       numero: 33,
+  //       cidade: "São Carlos",
+  //     },
+  //   },
+  // });
   //GET
   //Buscar usuários por endereço:
   // const usersByAddress = await prisma.user.findMany({
@@ -37,7 +35,6 @@ async function main() {
   //   },
   // });
   // console.log("Usuários com endereço contendo 'Flores':", usersByAddress);
-
   //DELETE
   //Excluir um usuário por e-mail:
   // await prisma.user.delete({
@@ -45,11 +42,30 @@ async function main() {
   //     email: "maria@exemplo.com",
   //   },
   // });
-
   //GET ALL
   // Buscar todos os usuários
-  const users = await prisma.user.findMany();
-  console.log("Todos os usuários:", users);
+  // const users = await prisma.user.findMany();
+  // console.log("Todos os usuários:", users);
+  //
+  //
+  // --------- Fazer a mesma coisa com Posts realicionados ao usuário ---------
+  //
+  //
+  //CREATE
+  // Criar um post
+  const post = await prisma.post.create({
+    data: {
+      title: "Meu primeiro post",
+      content: "Este é o conteúdo do meu primeiro post.",
+      author: {
+        connect: {
+          id: 2,
+          // email: "jose@gmail.com",
+        },
+      },
+    },
+  });
+  console.log("Post criado:", post);
 }
 
 main()
